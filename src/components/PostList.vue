bad021e6cb784ecd9ff80114e8b09fe1dd7b8112
Charit <charithnanduri03@gmail.com>
2022-03-02 19:44:42 -0500

    add posts to the home page

---
+++ src/components/PostList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-for="post in posts" :key="post">
+    <PostListItem :post="post" />
+    <span v-for="tag in post.tags" :key="tag">#{{tag}}</span>
+  </div>
+</template>
+
+<script>
+import PostListItem from "./PostListItem.vue";
+export default {
+  props: ["posts"],
+  components: { PostListItem },
+};
+</script>
+
+<style></style>
